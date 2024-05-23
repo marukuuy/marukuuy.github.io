e7f71ccc4ef5f68c6ca774ec2b276ce1370bb68a
maru <147112864+marukuuy@users.noreply.github.com>
2024-05-23 14:01:34 +0700

    initial commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function changeText() {
+    const text = document.getElementById("text").innerHTML;
+    if (text === "Nothing to see here") {
+        document.getElementById("text").innerHTML = "I told you";
+    } else if (text === "I told you") {
+        document.getElementById("text").innerHTML = "Well, if you want";
+    } else if (text === "Well, if you want") {
+        alert("Here's a cool MV from JKT48 for you 🙂");
+        window.location.href = "https://www.youtube.com/watch?v=mRMQsp90mFM";
+    }
+}
